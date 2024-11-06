ab684b5b09607cc66d9aedaee18736b0f22e8ee4
Ayush <ayushbhardwaj3746@gmail.com>
2024-11-07 00:32:07 +0530

    Update solution.js and add dotenv for safety

--- solution.js
+++ solution.js
@@ -2,15 +2,19 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
 const port = 3000;
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "permalist",
-  password: "1234",
-  port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 db.connect();
 
